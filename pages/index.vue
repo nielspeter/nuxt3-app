9969cb0a2efe9512be557a701cb43e45e57d7dc0
Niels Peter Strandberg <nielspeter@npstrandberg.com>
2021-12-27 22:01:47 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Home page</h1>
+    <NuxtLink to="/about">
+      About
+    </NuxtLink>
+  </div>
+</template>
---
+++ server/api/graphql.ts
@@ -0,0 +1,1 @@
+export default (_req, _res) => 'Hello World'
